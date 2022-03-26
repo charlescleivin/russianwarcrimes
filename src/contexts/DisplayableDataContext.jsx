b6a909f0ddd76de87fe431faf9174ecfc3cf2b85
Charles Cleivin <charlescleivin@gmail.com>
2022-03-26 17:13:03 -0300

    testing

--- src/contexts/DisplayableDataContext.jsx
+++ src/contexts/DisplayableDataContext.jsx
@@ -19,7 +19,7 @@ export const DisplayableDataContextProvider = (props) => {
             setfetchedFromApiArray(data)})   
         .catch((err)=>{console.log("Error:", err)})
     }, [])
-    
+    //
    
   let fetchedDataArray = [
       {          
