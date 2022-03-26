ac7a0353b5838a5bbf80bdd2f538489fe6375d2e
Charles Cleivin <charlescleivin@gmail.com>
2022-03-26 17:41:03 -0300

    now will work

--- src/contexts/DisplayableDataContext.jsx
+++ src/contexts/DisplayableDataContext.jsx
@@ -19,7 +19,7 @@ export const DisplayableDataContextProvider = (props) => {
             setfetchedFromApiArray(data)})   
         .catch((err)=>{console.log("Error:", err)})
     }, [])
-    //
+    //a
    
   let fetchedDataArray = [
       {          
